d4fd8167351c725a438be61c0d6973f11dbacdbf
courtneyjb7 <72003830+courtneyjb7@users.noreply.github.com>
2022-09-14 16:11:42 -0700

    update .env

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Monthly from "./components/spreads/Monthly";
 import Daily from "./components/spreads/Daily";
 
+
 function App() {
   return (
     <ChakraProvider>
